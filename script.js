c1cee08f512515f9875dce24f0123fa8dc84dad3
Vlad Metrik <vlad0707@gmail.com>
2016-09-02 13:22:06 -0700

    almost finished drawer

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-("nav").onclick = function(event) {
-	(".drawer").classList.add(".open");
+var toggleOpen = function(event) {	
+	
+	document.body.classList.toggle("open");
+
 };
+
+document.querySelector("nav").onclick = toggleOpen;
+
+document.querySelector(".close").onclick = toggleOpen;
